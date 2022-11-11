7621ef76ea9cf7bd4c8ee13dc80c98f882de9924
Phyoe Khant <pisces.pkz@gmail.com>
2022-11-11 17:49:19 +0900

    init

---
+++ storage/framework/views/22d8b309e1f76d9e6bc7f2047e61f26168e94d6b.php
@@ -0,0 +1,47 @@
+<?php $__env->startSection('content'); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">Contacts</div>
+                    <div class="card-body">
+                        <a href="<?php echo e(url('/employee/create')); ?>" class="btn btn-success btn-sm" title="Add New Contact">
+                            <i class="fa fa-plus" aria-hidden="true"></i> Add New
+                        </a>
+                        <br/>
+                        <br/>
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>Address</th>
+                                        <th>Telephone</th> 
+                                        <th>Photo</th> 
+                                </thead>
+                                </thead>
+                                <tbody>
+                                <?php $__currentLoopData = $employees; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <tr>
+                                        <td><?php echo e($loop->iteration); ?></td>
+                                        <td><?php echo e($item->name); ?></td>
+                                        <td><?php echo e($item->address); ?></td>
+                                        <td><?php echo e($item->mobile); ?></td>
+                                        <td>
+                                            <img src="<?php echo e(asset($item->photo)); ?>" width= '50' height='50' class="img img-responsive" />
+
+
+                                        </td>
+                                    </tr>
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('employees.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/woorom/workspace_github/my-admin-image-upload-app/resources/views/employees/index.blade.php ENDPATH**/ ?>
